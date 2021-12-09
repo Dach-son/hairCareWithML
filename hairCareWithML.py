eff77a5c456287ff72c5050512933f32272004c6
Dach-son <caideraid@gmail.com>
2021-12-08 23:49:41 -0600

    Minor Code Changes

--- hairCareWithML.py
+++ hairCareWithML.py
@@ -13,18 +13,12 @@ from tensorflow.keras.models import Sequential
 import pathlib
 
 #-- Loading the data for the code --#
-"""
-import pathlib
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
-data_dir = pathlib.Path(data_dir)
-"""
+data_dir = pathlib.Path("C:\\Users\\caide\\OneDrive\\Documents\\College\\6) Fall 2021\\Math 5001\\Project\\hairCareWithML\\photos")
 
 #-- Creating a dataset from the images provided --#
-"""
 batch_size = 32
-img_height = 180
-img_width = 180
+img_height = 1024
+img_width = 1024
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
@@ -41,31 +35,28 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   seed=123,
   image_size=(img_height, img_width),
   batch_size=batch_size)
-"""
+
+class_names = train_ds.class_names
+print(class_names)
 
 #-- Visualizing the data in a printable array --#
-"""
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(10, 10))
 for images, labels in train_ds.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
+  for i in range(25):
+    ax = plt.subplot(5, 5, i + 1)
     plt.imshow(images[i].numpy().astype("uint8"))
     plt.title(class_names[labels[i]])
     plt.axis("off")
-"""
 
 #-- Configuring the dataset for performance --#
-"""
 AUTOTUNE = tf.data.AUTOTUNE
 
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-"""
 
 #-- Standardize the data --#
-"""
 normalization_layer = layers.Rescaling(1./255)
 
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
@@ -73,10 +64,8 @@ image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
 # Notice the pixel values are now in `[0,1]`.
 print(np.min(first_image), np.max(first_image))
-"""
 
 #-- Creating the model --#
-"""
 num_classes = len(class_names)
 
 model = Sequential([
@@ -91,32 +80,24 @@ model = Sequential([
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
 ])
-"""
 
 #-- Compile the model --#
-"""
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-"""
 
 #-- Model Summary --#
-"""
 model.summary()
-"""
 
 #-- Train the model --#
-"""
 epochs=10
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs
 )
-"""
 
 #-- Visualize training results --#
-"""
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -138,10 +119,8 @@ plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
-"""
 
 #-- Data Augmentation --#
-"""
 data_augmentation = keras.Sequential(
   [
     layers.RandomFlip("horizontal",
@@ -160,10 +139,8 @@ for images, _ in train_ds.take(1):
     ax = plt.subplot(3, 3, i + 1)
     plt.imshow(augmented_images[0].numpy().astype("uint8"))
     plt.axis("off")
-"""
 
 #-- Dropout --#
-"""
 model = Sequential([
   data_augmentation,
   layers.Rescaling(1./255),
@@ -178,10 +155,8 @@ model = Sequential([
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
 ])
-"""
 
 #-- Compile and train the model --#
-"""
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
@@ -194,10 +169,8 @@ history = model.fit(
   validation_data=val_ds,
   epochs=epochs
 )
-"""
 
 #-- Visualize training results --#
-"""
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -219,10 +192,8 @@ plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
-"""
 
 #-- Predict on new data --#
-"""
 sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
 sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
 
@@ -239,5 +210,5 @@ print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(class_names[np.argmax(score)], 100 * np.max(score))
 )
-"""
+
 
